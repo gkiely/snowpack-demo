400f65339f8dbbdb38859b91d6761d28670e4e5d
Grant Kiely <gkiely@crunchyroll.com>
2020-12-15 21:33:42 -0800

    updated

--- snowpack.config.js
+++ snowpack.config.js
@@ -21,19 +21,16 @@ module.exports = {
     //     }
     //   }
     // ]
-    // TODO: fork repo and default to dependencies and version
-    // Also add exclude/ignore field, ignore: ["server-framework"] 
-    // And modules: ["react"]
-    // Remote extensions
     // TODO: get pinned URL's working
-    [
-      "snowpack-plugin-skypack-replacer", {
-        "modules": {
-          "react-dom": "17.0.1",
-          "react": "17.0.1"
-        },
-        "extensions": [".js", ".jsx"]
-      }
-    ]
+    "snowpack-plugin-skypack-replacer",
+    // [
+    //   "snowpack-plugin-skypack-replacer", {
+    //     "modules": {
+    //       "react-dom": "17.0.1",
+    //       "react": "17.0.1"
+    //     },
+    //     "extensions": [".js", ".jsx"]
+    //   }
+    // ]
   ]
 }
